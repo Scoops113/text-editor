469b53fed634c062e793a231c3ecac85e46f8c66
Stephen <stephencooper11395@gmail.com>
2024-07-26 11:56:14 -0500

    edits

--- server/server.js
+++ server/server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-  
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
